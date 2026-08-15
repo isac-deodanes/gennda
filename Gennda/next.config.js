1e9f6c9336c7512d6bb5d442c7c265dedc45a5bd
isac-deoanes <isacdeodanes@gmail.com>
2026-08-15 10:12:26 -0600

    cambios de direccion url de api

--- Gennda/next.config.js
+++ Gennda/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
         // Cuando en el frontend pidas http://127.0.0.1:8000/...
         source: '/api/:path*',
         // Vercel lo enviará a tu API de Render
-        destination: 'https://gennda-api.onrender.com/:path*', 
+        destination: 'https://gennda-api.onrender.com/api/:path*', 
       },
     ];
   },
